439b2ee36a5af9ea1c13fc6a2fd9ab024a28fc3c
lgybetter <437675103@qq.com>
2017-10-14 22:58:54 +0800

    fix wx js sdk

--- h5/src/App.vue
+++ h5/src/App.vue
@@ -10,15 +10,18 @@ import config from './config'
 import axios from 'axios'
 export default {
   name: 'app',
-  async create () {
+  async mounted () {
     const res = await axios.request({
       url: `${config.baseUrl}/api/wechat/getJSConfig`,
-      method: 'get'
+      method: 'get',
+      params: {
+        url: window.location.href
+      }
     })
-    console.log(res.data.data)
+    console.log(res.data.data, 'config')
     wx.config(res.data.data)
     wx.ready(function () {
-        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+      console.log('config success')// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
     })
     wx.error(function (res) {
       // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
