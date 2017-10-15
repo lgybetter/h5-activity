a9e49710b1ed5d142c8fe0f47fe484e0b64911b7
lgybetter <437675103@qq.com>
2017-10-15 14:09:15 +0800

    fix the audio change

--- services/core/audio-service.js
+++ services/core/audio-service.js
@@ -1,15 +1,49 @@
 import { wechatAPI } from '../../services/wechat/wechat-api'
 import fs from 'fs'
 import Path from 'path'
+import ffmpeg from 'fluent-ffmpeg'
+import cp from 'child_process'
+
+const changeAudioFormat = ({ name, type = '.amr',  path = Path.resolve(__dirname, '../../tempFiles'), output = Path.resolve(__dirname, '../../tempFiles') }) => {
+  return new Promise((resolve, reject) => {
+    const command = ffmpeg(`${path}/${name}${type}`)
+      .on('end', () => {
+        console.log('file has been converted succesfully');
+      })
+      .on('error', err => {
+        return reject(err)
+      })
+      .save(`${output}/${name}.mp3`);
+    return resolve({
+      path: output,
+      type: '.mp3',
+      name
+    })
+  })
+}
+
+const mergeAudio = (audio1, audio2, output = '') => {
+  return new Promise((resolve, reject) => {
+    cp.exec(`ffmpeg -i "concat:${audio1}|${audio2}" -acodec copy ${output}`, (err) => {
+      if(err) {
+        return reject(err)
+      }
+      return resolve(output)
+    })
+  })
+}
 
 const changeMedia = (buffer, { path = Path.resolve(__dirname, '../../tempFiles'), name = '', type = '.amr' }) => {
-  console.log(path)
   return new Promise((resolve, reject) => {
     fs.writeFile(`${path}/${name}${type}`, buffer, (err) => {
       if(err) {
         return reject(err)
       }
-      return resolve(`${name}${type}`)
+      return resolve({
+        path,
+        name,
+        type
+      })
     })
   })
 }
@@ -18,16 +52,9 @@ const getMedia = mediaId => {
   return new Promise((resolve, reject) => {
     wechatAPI.getMedia(mediaId, async (err, result, res) => {
       if(err) {
-        console.log(err)
         return reject(err)
       }
-      console.log(result, 'change media')
-      try {
-        const file = await changeMedia(result, { name: mediaId }) 
-        console.log(file, 'changeFile success')
-      } catch (error) {
-        console.log(error)
-      }
+      const file = await changeMedia(result, { name: mediaId }) 
       return resolve({
         data: result,
         res
