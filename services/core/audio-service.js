f80339a7b2703b97328da4c910100abb5420dda3
lgybetter <437675103@qq.com>
2017-10-15 10:59:00 +0800

    add the change audio buffer to file

--- services/core/audio-service.js
+++ services/core/audio-service.js
@@ -1,13 +1,31 @@
 import { wechatAPI } from '../../services/wechat/wechat-api'
+import fs from 'fs'
+
+const changeMedia = (buffer, { path = '../../tempFiles', name = '', type = '.amr' }) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(`${path}/${name}/${type}`, buffer, (err) => {
+      if(err) {
+        return reject(err)
+      }
+      return resolve(`${path}/${name}/${type}`)
+    })
+  })
+}
 
 const getMedia = mediaId => {
   return new Promise((resolve, reject) => {
-    wechatAPI.getMedia(mediaId, (err, result, res) => {
+    wechatAPI.getMedia(mediaId, async (err, result, res) => {
       if(err) {
         console.log(err)
         return reject(err)
       }
-      console.log(res, 'media')
+      console.log(result, 'change media')
+      try {
+        const file = await changeMedia(result, { name: mediaId }) 
+        console.log(file, 'changeFile success')
+      } catch (error) {
+        console.log(error)
+      }
       return resolve({
         data: result,
         res
@@ -15,7 +33,7 @@ const getMedia = mediaId => {
     })
   })
 }
-
 export {
-  getMedia
+  getMedia,
+  changeMedia
 }
