22ef1a32336875fa12606e6f0c0946c1f0ef6ae7
lgybetter <437675103@qq.com>
2017-10-15 13:39:12 +0800

    add the ffmpeg

--- services/core/audio-service.js
+++ services/core/audio-service.js
@@ -2,10 +2,10 @@ import { wechatAPI } from '../../services/wechat/wechat-api'
 import fs from 'fs'
 import Path from 'path'
 
-const changeMedia = (buffer, { path = Path.resolve(__dirname, '../../tempFiles'), name = '', type = '.amr' }) => {
+const changeMedia = (buffer, { path = './tempFiles', name = '', type = '.amr' }) => {
   console.log(path)
   return new Promise((resolve, reject) => {
-    fs.writeFile(`${name}${type}`, buffer, (err) => {
+    fs.writeFile(`${path}/${name}${type}`, buffer, (err) => {
       if(err) {
         return reject(err)
       }
